b9f1686a846c541db01bf660b80b9755e66dbac2
tommypacker <tommypacker@gmail.com>
2018-09-26 21:36:09 -0700

    Add more routes

--- rankr/main/controllers.py
+++ rankr/main/controllers.py
@@ -1,3 +1,5 @@
+import rankr.constants as constants
+
 from flask import current_app, jsonify, Blueprint
 from rankr.analyzer import Analyzer
 from rankr.dbconnector import MongoConnector
@@ -6,6 +8,8 @@ from rankr.dbconnector import MongoConnector
 main = Blueprint('main', __name__)
 rankrDB = MongoConnector()
 
+#TODO: Add input validation
+
 @main.route('/')
 def index():
 	return "Rankr"	
@@ -13,9 +17,22 @@ def index():
 @main.route('/stats/<int:week>/')
 def all_stats(week):
 	a = Analyzer(current_app.config["LEAGUE_ID"], rankrDB)
+	res = a.get_all_analyst_errors(week)
+	return jsonify(res)
+
+@main.route('/stats/<int:week>/<int:position>/')
+def position_stats(week, position):
+	a = Analyzer(current_app.config["LEAGUE_ID"], rankrDB)
+	res = a.get_all_analyst_errors(week)
+	position = constants.POSITION_MAP[position]
+	return jsonify(res[position])
+
+@main.route('/stats/aggregated/<int:week>/')
+def aggregated_stats(week):
+	a = Analyzer(current_app.config["LEAGUE_ID"], rankrDB)
 	res = a.get_aggregated_analyst_errors(week)
 	return jsonify(res)
 
-@main.route('/stats/<int:week>/<int:position>')
-def position_stats(week, position):
-	return "Errors for week {} position {}".format(week, position)
+@main.route('/stats/aggregated/season/')
+def aggregated_season_stats():
+	return "Season"
