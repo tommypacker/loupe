bc208f61a6e721f7a73f7067f99e9a29d3f8a2d1
tommypacker <tommypacker@gmail.com>
2018-11-23 14:12:35 -0800

    Sort position groups

--- rankr/static/js/charts.js
+++ rankr/static/js/charts.js
@@ -1,35 +1,41 @@
-const margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+const POSITIONS = ["QB", "RB", "WR", "TE", "DST", "K"];
+const ANALYSTS = ["BERRY", "KARABELL", "YATES", "COCKROFT", "CLAY", "BELL"]
+const margin = {top: 20, right: 20, bottom: 30, left: 40};
+const width = 960 - margin.left - margin.right;
+const height = 500 - margin.top - margin.bottom;
+const colors = d3.scaleOrdinal()
+    .range(["#ca0020","#f4a582","#d5d5d5","#92c5de","#0571b0", "#90EE90"]);
+
+// Create basis for bar charts
 var x0 = d3.scaleBand()
     .range([0, width])
     .paddingInner(0.2);
 var x1 = d3.scaleBand();
 var y = d3.scaleLinear()
     .range([height, 0]);
-const colors = d3.scaleOrdinal()
-    .range(["#ca0020","#f4a582","#d5d5d5","#92c5de","#0571b0", "#90EE90"]);
-
 var svg = d3.select('body').append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+/*
+Creates a grouped bar chart that displays the errors for each analyst
+for each position in a given week. The lower the error, the better
+and analyst did in predicting how well a player did.
+*/
 function loadChart(week) {
-    // Remove existing charts
+    // Remove existing data
     svg.selectAll("*").remove();
 
     url = "http://localhost:5000/stats/" + week + "/"
     d3.json(url).then(function(data) {
-        var positions = Object.keys(data);
-        var analysts = Object.keys(data.QB);
-        data = formatData(data, positions, analysts);
-        var minVal = d3.min(formattedData, function(pos) { return d3.min(pos.analysts, function(d) { return d.value; }); });
-        var maxVal = d3.max(formattedData, function(pos) { return d3.max(pos.analysts, function(d) { return d.value; }); });
+        data = formatData(data);
+        var minVal = d3.min(formattedData, function(pos) { return d3.min(pos.ANALYSTS, function(d) { return d.value; }); });
+        var maxVal = d3.max(formattedData, function(pos) { return d3.max(pos.ANALYSTS, function(d) { return d.value; }); });
 
-        x0.domain(positions);
-        x1.domain(analysts).range([0, x0.bandwidth()]);
+        x0.domain(POSITIONS);
+        x1.domain(ANALYSTS).range([0, x0.bandwidth()]);
         y.domain([minVal - 0.02, maxVal + 0.02]);
 
         var xAxis = d3.axisBottom().scale(x0).tickSize(0);
@@ -61,7 +67,7 @@ function loadChart(week) {
             .attr("transform",function(d) { return "translate(" + x0(d.position) + ",0)"; });
 
         slice.selectAll("g")
-            .data(function(d) { return d.analysts; })
+            .data(function(d) { return d.ANALYSTS; })
         .enter().append("rect")
             .attr("width", x1.bandwidth())
             .attr("x", function(d) { return x1(d.analyst); })
@@ -73,7 +79,7 @@ function loadChart(week) {
 
         //Legend
         var legend = svg.selectAll(".legend")
-        .data(formattedData[0].analysts.map(function(d) { return d.analyst; }))
+        .data(formattedData[0].ANALYSTS.map(function(d) { return d.analyst; }))
         .enter().append("g")
         .attr("class", "legend")
         .attr("transform", function(d,i) { return "translate(0," + i * 20 + ")"; })
@@ -96,14 +102,14 @@ function loadChart(week) {
     });
 }
 
-function formatData(data, positions, analysts) {
+function formatData(data) {
     formattedData = []
-    positions.map(function (pos) {
+    POSITIONS.map(function (pos) {
         var newEntry = {};
         newEntry["position"] = pos;
-        newEntry["analysts"] = [];
-        analysts.map(function (analyst) {
-            newEntry["analysts"].push({analyst: analyst, value: data[pos][analyst]});
+        newEntry["ANALYSTS"] = [];
+        ANALYSTS.map(function (analyst) {
+            newEntry["ANALYSTS"].push({analyst: analyst, value: data[pos][analyst]});
         });
         formattedData.push(newEntry);
     });
